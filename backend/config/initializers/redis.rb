0915261c04967bf10faa1cf7136006ac7266b226
Matthew Bechtel <bechma02@gmail.com>
2025-04-05 23:00:40 -0700

    Redis initializer supports heroku deployment

--- backend/config/initializers/redis.rb
+++ backend/config/initializers/redis.rb
@@ -3,7 +3,7 @@ $redis = Redis.new(
     port: ENV["REDIS_PORT"],
     # password: Figaro.env.redis_password, 
     db: ENV["REDIS_DB"],
-    # ssl_params: {
-    #     verify_mode: OpenSSL::SSL::VERIFY_NONE
-    # }
+    ssl_params: {
+        verify_mode: OpenSSL::SSL::VERIFY_NONE
+    }
 )
